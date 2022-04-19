9d1a972a29193c776a44c703f1dd4f88dddb8559
Abraham James <horlamidex1@gmail.com>
2022-04-19 14:00:30 -0700

    Updated main.h with functions and helper functions

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - entry point of printf like function
+ * @format: argument that specify the format type
+ * @...: variable length argument
+ **/
+
+int _printf(const char *format, ...)
+{
+  
+}
